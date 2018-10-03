de3df205bbb70176921cbab7c0df83c607ef604c
D Spafford <spafford@dereks-mac-mini.local>
2018-10-03 13:27:50 +0100

    Add new item functionality complete.

--- SPELLS/SpellingItemsViewController.swift
+++ SPELLS/SpellingItemsViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class SpellingItemsViewController: UITableViewController {
 
-    let itemArray = ["light", "floor", "key", "free"]
+    var itemArray = ["light", "floor", "key", "free"]
     
     override func viewDidLoad() {
         
@@ -54,6 +54,39 @@ class SpellingItemsViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
         
+        }
+    
+    //MARK: - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+       var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Spelling Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            //what will happen once the user clicks the Add Item button on our UIAlert
+            
+         self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+            
+        }
+       
+        alert.addTextField { (alertTextField) in
+            
+            alertTextField.placeholder = "Create New Item"
+            
+            textField = alertTextField
+            
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
     }
+    
 }
 
